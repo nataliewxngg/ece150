02da45bed1c6c35bb9bb5760db3575e682ed0eb5
natalie <wong.lt.natalie@gmail.com>
2024-09-05 16:35:16 -0400

    L1.01-1.03

--- L1/Main.cpp
+++ L1/Main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
+// #include <math>
 
-int main(); // semicolon!
+// Function declarations
+int main();
 
+// Function definitions
 int main()
 {
-    std::cout << "Hello ECE 150!" << std::endl;
 
+    // Lesson 1.01
+    // Display this text to the console
+    std::cout << "Hello World!"; // cout: console out
+    std::cout << std::endl;      // endl: end line
+
+    // ----------------------------------------------------
+
+    // Lesson 1.02
     int course{150}; // a var named course
     std::cout << "Hello ECE " << course << " meow" << std::endl;
+
+    // declares an int function gcd with 2 int parameters, m and n
+    int gcd(int m, int n);
+
+    // ----------------------------------------------------
+
+    // Lesson 1.03
+    std::cout << "The answer to the ultimate question is ";
+    std::cout << 42;
+    std::cout << std::endl;
+
+    std::cout << 'a';
+    std::cout << 'b';
+    std::cout << 'c';
+    std::cout << std::endl;
+
+    std::cout << "\' \\"; // escape sequence
 
     return 0; // to end program
 }
