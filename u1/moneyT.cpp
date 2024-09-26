f4b9d03a8c097e33a187c3cf9dd76a24932e43a4
Natalie Wong <wong.lt.natalie@gmail.com>
2024-09-26 17:12:20 -0400

    MoneyT
    Example from ECE 150 Tutorial

---
+++ u1/moneyT.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cassert>
+
+int main() {
+    int n;
+
+    std::cout<<"Enter a size: ";
+    std::cin>>n;
+    assert(n%2!=0 && n>=3);
+
+    for (int i{0};i<n;++i) {
+        std::cout<<"$";
+    } 
+    std::cout<<std::endl;
+
+    for (int i{0};i<n-1;++i) {
+        for (int j{0};j<(n/2);++j){
+            std::cout<<" ";
+        }
+        std::cout<<"$"<<std::endl;
+    }
+
+    return 0;
+}
