8c0eedc50e4a8e5b71bd9ca698e650d397c17638
Natalie Wong <wong.lt.natalie@gmail.com>
2024-09-26 17:11:49 -0400

    Prime Factor Decomposition
    Example from ECE 150 Tutorial

---
+++ u1/L25_binaryAndHexadecimalNumbers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int x = 0xb;
+    int y = 0b1011;
+
+    std::cout<<"x: "<<x<<std::endl; // 11
+    std::cout<<"y: "<<y<<std::endl; // 11
+}
---
+++ u1/primeFactorDecomposition.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cassert>
+
+int main() {
+    int in;
+    std::cout<<"Enter a number: ";
+    std::cin>>in;
+    assert(in>1);
+
+    while (in>1) {
+        int divisor{2};
+        int exponent{0};
+
+        while(in%divisor!=0) {
+            divisor++;
+        }
+        while (in % divisor == 0) {
+            exponent++;
+            in /= divisor;
+        }
+
+        std::cout<<divisor<<"^"<<exponent;
+        if (in>1) {
+            std::cout<<" * ";
+        }
+    }
+    std::cout<<std::endl;
+
+    return 0;
+}
