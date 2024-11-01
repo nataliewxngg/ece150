0c728464757b9d95b56b2ebc55b3a97880bd214b
nataliewxngg <wong.lt.natalie@gmail.com>
2024-10-31 21:39:13 -0400

    2.02 Dynamic Memory Allocation
    Covered the new and delete operators

---
+++ u2/L2_dynamicAddressAllocation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main();
+int main()
+{
+    int *p_int{};
+    p_int = new int{2};
+    std::cout << p_int << std::endl;
+
+    // or
+
+    int *p_int1{new int{2}};
+
+    // Unallocating memory to the program
+    delete p_int;
+    p_int = nullptr; // resets the value stored in the once allocated memory to be null
+
+    return 0;
+}
