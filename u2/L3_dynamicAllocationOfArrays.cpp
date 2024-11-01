601eeebcce009b8ee309bf4568a3d111d0ab4a03
nataliewxngg <wong.lt.natalie@gmail.com>
2024-10-31 22:17:10 -0400

    Updated with a Function for Dynamically Allocated Arrays

--- u2/L3_dynamicAllocationOfArrays.cpp
+++ u2/L3_dynamicAllocationOfArrays.cpp
@@ -3,6 +3,19 @@
 // return locations of arrays = a "hack" to return arrays
 // int/double/... *array is another way of passing an array into a function
 
+// eg.
+// Method to clear (and deallocate - usually not included in method) a DYNAMICALLY allocated array
+// (will not work for local arrays - these cannot be deallocated)
+void deallocate(double *arr, const std::size_t cap)
+{
+    for (std::size_t k{0}; k < cap; ++k)
+    {
+        arr[k] = 0.0;
+    }
+    delete[] arr;
+    arr = nullptr;
+}
+
 int main();
 int main()
 {
