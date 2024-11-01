4c7757ab6122988c95556aaaba3d2b2fb34e5f95
nataliewxngg <wong.lt.natalie@gmail.com>
2024-10-31 20:57:51 -0400

    2.01 Addresses and Pointers

---
+++ project_3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cassert>
+
+int main();
+int main()
+{
+    return 0;
+}
+
+// Method #1
+double *geometric(double a, double ratio, std::size_t cap)
+{
+}
+
+// Method #2
+double *cross_correlation(
+    double array0[], std::size_t cap0, double array1[], std::size_t cap1)
+{
+}
+
+// Method #3
+std::size_t shift_duplicates(int array[], std::size_t capacity)
+{
+}
+
+// Method #4
+void deallocate(double *&ptr, bool is_array, std::size_t capacity = 0)
+{
+}
---
+++ u2/L1_addressesAndPointers.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main();
+int main()
+{
+    // Pointers
+    // Df: Variables that store an address (naming convention: "p_" prefix)
+
+    int a{1};
+    int arr_a[10];
+    int *p_a{&a}; // Stores address of int a
+    // <- & operator is used to get the addresses of local variables/parameters
+
+    int *p_arr_a{arr_a}; // Stores address of int array arr_a
+
+    std::cout << a << std::endl;
+    std::cout << p_a << std::endl; // Address of int a
+
+    // These two are the same
+    std::cout << arr_a << std::endl;
+    std::cout << arr_a << std::endl;
+
+    // Accessing and modifying the variable stored in a pointer:
+    int n{2};
+    int *p_n{&n};
+
+    *p_n = 3;
+    std::cout << n << std::endl;    // 3
+    std::cout << *p_n << std::endl; // 3
+
+    // std::size_t <- unsigned # that can store the maximum index for a particular processor (a universal solution to the varying types of indexes to arrays)
+    const std::size_t cap = 20;
+    int arr_b[cap];
+
+    return 0;
+}
