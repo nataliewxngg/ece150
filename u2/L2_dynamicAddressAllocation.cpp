7573b295f93ac65ba846d22bae9a381dcf23f12d
nataliewxngg <wong.lt.natalie@gmail.com>
2024-10-31 21:40:31 -0400

    Update to 2.02

--- u2/L2_dynamicAddressAllocation.cpp
+++ u2/L2_dynamicAddressAllocation.cpp
@@ -12,8 +12,8 @@ int main()
     int *p_int1{new int{2}};
 
     // Unallocating memory to the program
-    delete p_int;
-    p_int = nullptr; // forget this address
+    delete p_int;    // unallocates the address
+    p_int = nullptr; // forget this address - by setting it to the zero address
 
     return 0;
 }
