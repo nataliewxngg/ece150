94c420be5d4194e6de4bbfe0c79fcd0be27ba7f7
natalie <wong.lt.natalie@gmail.com>
2024-11-16 14:11:31 -0500

    4.05 Issues with Member Variables

---
+++ u4/L5_issuesWithMemberVariables.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main();
+int main()
+{
+    // Normal Form
+    // it is of best practice to "normalize" objects - so that data is consistent and not redundant
+    // (so that objects with different parameters that are essentially the same are represented the same way)
+
+    // ------------------------------------------
+
+    // To avoid problems (eg. wild pointers, null pointers, memory leaks),
+    // deal with the creation of the object from CREATION, EXISTENCE, to DESTRUCTION
+
+    // Terminology:
+    // 1. Constructors... deal with initialization
+    // 2. Member Functions... deal with accessing and manipulating
+    // 3. Destructors... deal with clean-up (ie. deallocation)
+
+    // ------------------------------------------
+
+    // Encapsulation: protection of data (!= security)
+
+    return 0;
+}
